7cde0501538ed139c486176febaf023ff8e561db
Sunreys21 <jumierreycabanero@gmail.com>
2025-06-01 04:32:18 +0800

    Practice portfolio

---
+++ src/components/Home/index.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react' // <-- add this import
+import LogoTitle from '../../assets/images/logo-s.png';
+import { Link } from 'react-router-dom';
+import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
+import Loader from 'react-loaders';
+
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['u', 'm', 'i', 'e', 'r']
+    const jobArray = ['F', 'u', 'l', 'l', 's', 't', 'a','c','k',' ',
+                    'D', 'e', 'v', 'e', 'l', 'o','p','e','r']
+
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+
+        return () => clearTimeout(timeoutId);
+    }, []);
+
+
+    return (
+        <>
+        <div className ="container home-page">
+            <div className="text-zone">
+                <h1>
+                <span className={letterClass}>H</span>    
+                <span className={`${letterClass} _12`}>i,</span>    
+                <br /> 
+                <span className={`${letterClass} _13`}>I</span>  
+                <span className={`${letterClass} _14`}>'m</span>  
+                <img src={LogoTitle} alt="developer" />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx = {15}/>
+                <br />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx = {22}/>
+                </h1>
+                <h2>Java / JavaScript Expert / C / C++</h2>
+                <h2>2nd Year BS Computer Engineering Student</h2>
+                <Link to ="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+            <Logo />
+        </div>
+            <Loader type="pacman" />
+        </>
+    )
+}
+
+export default Home
---
+++ src/components/Layout/index.js
@@ -0,0 +1,25 @@
+import './index.scss';
+import Sidebar from '../Sidebar';
+import { Outlet } from 'react-router-dom';
+
+
+const Layout = () => {
+    return (
+    <div className="App">
+        <Sidebar/>
+        <div className='page'>
+            <span className='tags top-tags'>System.out.println</span>
+
+            <Outlet/>
+            
+            <span className='tags bottom-tags'>
+                sc.close();
+                <br /> 
+            <span className = 'bottom-tag-html'></span>
+            </span>
+        </div>
+    </div>
+    )
+}
+
+export default Layout
